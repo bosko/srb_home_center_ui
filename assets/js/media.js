0b26af2aab6005d7f64dd3c441bf1e08a2e2aa88
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2020-11-11 20:18:24 +0100

    feat: use action icons instead of popup menu

--- assets/js/media.js
+++ assets/js/media.js
@@ -3,15 +3,15 @@ import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/light-border.css";
 
 document.addEventListener("DOMContentLoaded", function() {
-  tippy('.queue-song', {
-    allowHTML: true,
-    interactive: true,
-    trigger: "click",
-    theme: "light-border",
-    content(reference) {
-      const index = reference.getAttribute("data-queue-index");
-      const template = document.getElementById("song-popup-template");
-      return template.innerHTML.replaceAll("{song-index}", index)
-    }
-  })
-});
+  // tippy('.queue-song', {
+  //   allowHTML: true,
+  //   interactive: true,
+  //   trigger: "click",
+  //   theme: "light-border",
+  //   content(reference) {
+  //     const index = reference.getAttribute("data-queue-index");
+  //     const template = document.getElementById("song-popup-template");
+  //     return template.innerHTML.replaceAll("{song-index}", index)
+  //   }
+  // })
+})
