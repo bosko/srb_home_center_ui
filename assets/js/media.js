23e9dc567a6ad6036b3d85954ec4f25748230123
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2020-12-15 16:19:44 +0100

    feat: removed/commented out tippy part

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,17 +1,6 @@
-import tippy from "tippy.js"
-import "tippy.js/dist/tippy.css";
-import "tippy.js/themes/light-border.css";
+// import tippy from "tippy.js"
+// import "tippy.js/dist/tippy.css";
+// import "tippy.js/themes/light-border.css";
 
 document.addEventListener("DOMContentLoaded", function() {
-  // tippy('.queue-song', {
-  //   allowHTML: true,
-  //   interactive: true,
-  //   trigger: "click",
-  //   theme: "light-border",
-  //   content(reference) {
-  //     const index = reference.getAttribute("data-queue-index");
-  //     const template = document.getElementById("song-popup-template");
-  //     return template.innerHTML.replaceAll("{song-index}", index)
-  //   }
-  // })
 })
