57ca5d8b65a56b28f2ab532aab556ea026588198
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2021-01-03 10:48:50 +0100

    feat: mobile displaying song actions

--- assets/js/media.js
+++ assets/js/media.js
@@ -3,4 +3,24 @@
 // import "tippy.js/themes/light-border.css";
 
 document.addEventListener("DOMContentLoaded", function() {
+  let songInfos = document.querySelectorAll(".song-info")
+
+  songInfos.forEach(function(songInfo) {
+    songInfo.addEventListener("click", function(e) {
+      e.preventDefault()
+      e.stopPropagation()
+
+      if (this.parentNode.classList.contains('shown-actions')) {
+        this.parentNode.classList.remove("shown-actions")
+        this.parentNode.classList.add("hidden-actions")
+
+        this.parentNode.previousElementSibling.className = "inactive-mobile-actions"
+      } else {
+        this.parentNode.classList.add("shown-actions")
+        this.parentNode.classList.remove("hidden-actions")
+
+        this.parentNode.previousElementSibling.className = "active-mobile-actions"
+      }
+    })
+  })
 })
